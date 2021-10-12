13d09517d5b5c112448af79803f9171f79dbb0b4
wang-er <33679181+wang-er@users.noreply.github.com>
2021-10-11 22:51:03 -0400

    oml gitignore pls

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-styled-components/gatsby-browser.js'),
-      options: {"plugins":[],"displayName":true,"fileName":true,"minify":true,"namespace":"","transpileTemplateLiterals":true,"topLevelImportPaths":[],"pure":false,"disableVendorPrefixes":false},
-    },{
-      plugin: require('../node_modules/gatsby-plugin-image/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
